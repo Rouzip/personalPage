b17293ae6c46a0a0c0c60cdb6f47a334e293d843
Rouzip <1226015390@qq.com>
2018-05-17 10:03:27 +0800

    尝试计算属性

--- webVue/src/compoments/info.vue
+++ webVue/src/compoments/info.vue
@@ -10,21 +10,24 @@
     </el-carousel>
     <div class="middle-box">
       <div class="pictures"
-            v-for="index in 3"
-            :key="index">
-        <img :src="'static/'+getPic(picturesIndex[index-1])" alt="教师图片" class="avatar">
+            v-if="teachers.length !== 0"
+            v-for="index in teachers.length"
+            :key="index"
+            v-show="showJudge(index-1)">
+        <!-- TODO: 使用一个boolean的数组来判断是否展示这个item，用pictureIndex来记录展示的3个索引 -->
+        <img :src="'static/'+teacherInfo(index-1, 'picture')" alt="教师图片" class="avatar">
         <div class="info">
           <div class="texts">
-            教师姓名：{{teacherInfo(picturesIndex[index-1], 'name')}}
+            教师姓名：{{teacherInfo(index-1, 'name')}}
           </div>
           <div class="texts">
-            单位：{{teacherInfo(picturesIndex[index-1], 'department')}}
+            单位：{{teacherInfo(index-1, 'department')}}
           </div>
           <div class="texts">
-            学位：{{teacherInfo(picturesIndex[index-1], 'degree')}}
+            学位：{{teacherInfo(index-1, 'degree')}}
           </div>
           <div class="texts">
-            职称：{{teacherInfo(picturesIndex[index-1], 'position')}}
+            职称：{{teacherInfo(index-1, 'position')}}
           </div>
         </div>
       </div>
@@ -51,33 +54,47 @@ export default {
         "background-repeat": "no-repeat"
       };
     },
-    getPic: function (index) {
-      if (this.$store.state.teachers.length === 0){
-        return
-      }
-      let pictures = []
-      this.picturesIndex.forEach(index => {
-        pictures.push(this.$store.state.teachers[index].picture)
-      })
-      return pictures[index]
-    },
     teacherInfo: function (index, key) {
-      if (this.$store.state.teachers.length === 0) {
-        return ''
+      // 获取teacher的信息
+      try {
+        if (this.$store.state.teachers.length === 0) return
+      return this.teachers[index][key]
+      } catch (error) {
+        console.log(error)
       }
-      return this.$store.state.teachers[index][key]
     },
     addIndex:function(path){
-      console.log('触发')
+      // 跟随carsouel的change触发的函数
       if (this.$store.state.teachers.length === 0) return
       for (let i=0;i<3;i++) {
         this.picturesIndex[i] += 3
         this.picturesIndex[i] %= this.$store.state.teachers.length
       }
+    },
+    showJudge(index) {
+      // 判断是否可以展示
+      if (index in this.picturesIndex) return true
+      else return false
     }
   },
   computed: {
-    
+    teachers: function() {
+      if (this.$store.state.teachers.length === 0) return []
+      else return this.$store.state.teachers
+    },
+    showArray: function(picturesIndex) {
+      if (this.$store.state.teachers.length === 0) {
+        return []
+      }
+      else {
+        let res = []
+        for (let i=0;i<this.teachers.length;i++) {
+          if (i in this.picturesIndex) res.push(true)
+          else res.push(false)
+        }
+        return res
+      }
+    }
   }
 };
 </script>
