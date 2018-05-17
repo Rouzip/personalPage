8df95334a19edfc33fd4f6c8eb0bdf4f0dcdee81
Rouzip <1226015390@qq.com>
2018-05-17 08:48:19 +0800

    尝试多个图预加载

--- webVue/src/compoments/info.vue
+++ webVue/src/compoments/info.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="banner">
-    <el-carousel height="479px">
+    <el-carousel height="479px" @change="addIndex">
       <el-carousel-item v-for="index in 3" 
                         :key="index" 
                         :style="styleItem(index)" 
                         autoplay
-                        interval='1000'>
+                        interval='2000'>
       </el-carousel-item>
     </el-carousel>
     <div class="middle-box">
       <div class="pictures"
             v-for="index in 3"
             :key="index">
-        <img :src="'static/'+getPic(index-1)" alt="教师图片" class="avatar">
+        <img :src="'static/'+getPic(picturesIndex[index-1])" alt="教师图片" class="avatar">
+        <div class="info">
+          <div class="texts">
+            教师姓名：{{teacherInfo(picturesIndex[index-1], 'name')}}
+          </div>
+          <div class="texts">
+            单位：{{teacherInfo(picturesIndex[index-1], 'department')}}
+          </div>
+          <div class="texts">
+            学位：{{teacherInfo(picturesIndex[index-1], 'degree')}}
+          </div>
+          <div class="texts">
+            职称：{{teacherInfo(picturesIndex[index-1], 'position')}}
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -29,6 +43,7 @@ export default {
   },
   methods: {
     styleItem: function(index) {
+      // 此函数用来生成背景的url
       return {
         "background-image":
           "url(" + require("../assets/" + index + ".jpg") + ")",
@@ -45,9 +60,24 @@ export default {
         pictures.push(this.$store.state.teachers[index].picture)
       })
       return pictures[index]
+    },
+    teacherInfo: function (index, key) {
+      if (this.$store.state.teachers.length === 0) {
+        return ''
+      }
+      return this.$store.state.teachers[index][key]
+    },
+    addIndex:function(path){
+      console.log('触发')
+      if (this.$store.state.teachers.length === 0) return
+      for (let i=0;i<3;i++) {
+        this.picturesIndex[i] += 3
+        this.picturesIndex[i] %= this.$store.state.teachers.length
+      }
     }
   },
   computed: {
+    
   }
 };
 </script>
@@ -56,8 +86,8 @@ export default {
   background-repeat: no-repeat;
 }
 .pictures{
-  display: -webkit-inline-flex;
-  width: 30%;
+  display: flex;
+  width: 20%;
   height: 100%;
 }
 .middle-box {
@@ -67,13 +97,25 @@ export default {
   top: 35%;
   height: 200px;
   width: 100%;
-  background: #333333;
-  justify-content: space-between;
+  justify-content: space-around;
 }
 .avatar {
   overflow: hidden;
   width: 100%;
   height: 100%;
   float: left;
+}
+.info {
+  position: absolute;
+  bottom: 0%;
+  height: 50%;
+  width: 20%;
+  opacity: 0.9;
+  flex-direction: column;
+  background: #3d3e3f;
+  justify-content: space-between;
+}
+.texts {
+  color: aliceblue;
 }
 </style>
