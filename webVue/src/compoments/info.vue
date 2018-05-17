65c47e46c1c2a29d9b578b2d8c6b19df90dd02b8
Rouzip <1226015390@qq.com>
2018-05-17 11:20:59 +0800

    初始页完毕

--- webVue/src/compoments/info.vue
+++ webVue/src/compoments/info.vue
@@ -13,8 +13,10 @@
             v-if="teachers.length !== 0"
             v-for="index in teachers.length"
             :key="index"
-            v-show="showJudge(index-1)">
+            v-show="showArray[index-1]">
         <!-- TODO: 使用一个boolean的数组来判断是否展示这个item，用pictureIndex来记录展示的3个索引 -->
+        <!-- FIXME:现在的bug是思路问题，如果把是否在数组中定为函数，无法触发重新判断，但是放在计算属性中，一开始没法初始化 -->
+        <!-- 方向1：change事件触发之后，更新pictureIndex，watch更新showArray -->
         <img :src="'static/'+teacherInfo(index-1, 'picture')" alt="教师图片" class="avatar">
         <div class="info">
           <div class="texts">
@@ -41,7 +43,8 @@ export default {
     return {
       // 这里使用索引来进行picture的展示
       // 在父组件进行循环的时候，动态的改变三个索引值来改变图像？
-      picturesIndex: [0, 1, 2]
+      picturesIndex: [0, 1, 2],
+      showArray: [true, true, true, false, false, false]
     };
   },
   methods: {
@@ -66,34 +69,45 @@ export default {
     addIndex:function(path){
       // 跟随carsouel的change触发的函数
       if (this.$store.state.teachers.length === 0) return
+      // 这里只打算展示3张，所以用做三次循环
       for (let i=0;i<3;i++) {
-        this.picturesIndex[i] += 3
-        this.picturesIndex[i] %= this.$store.state.teachers.length
+        let newValue = (this.picturesIndex[i] + 3) % this.$store.state.teachers.length
+        this.$set(this.picturesIndex, i, newValue)
+      }
+      // trick 不搞observe，直接用数组
+      let tmp = []
+      this.picturesIndex.forEach(index=>{
+        tmp.push(index)
+      })
+      // for(const v in tmp)
+        console.log(tmp)
+      for (let i=0;i<this.teachers.length;i++) {
+        if (tmp.indexOf(i) !== -1) {
+          console.log(i)
+          this.$set(this.showArray, i, true)
+          // this.showArray[i] = true
+        }
+        else {
+          this.$set(this.showArray, i, false)
+        }
       }
     },
     showJudge(index) {
       // 判断是否可以展示
       if (index in this.picturesIndex) return true
       else return false
+    },
+    updateShowArray() {
+      for (let i=0;i<this.teachers.length;i++) {
+        if (i in this.picturesIndex) this.showArray[i] = true
+        else this.showArray[i] = false
+      }
     }
   },
   computed: {
     teachers: function() {
       if (this.$store.state.teachers.length === 0) return []
       else return this.$store.state.teachers
-    },
-    showArray: function(picturesIndex) {
-      if (this.$store.state.teachers.length === 0) {
-        return []
-      }
-      else {
-        let res = []
-        for (let i=0;i<this.teachers.length;i++) {
-          if (i in this.picturesIndex) res.push(true)
-          else res.push(false)
-        }
-        return res
-      }
     }
   }
 };
